4b4e7214bc6c330c45f6148552119c8f7b9dcace
claudiotilbe <mo.claudio-26@hotmail.com>
2024-09-04 17:43:38 -0300

    Limpieza

--- OFA - Proyecto/OFA - Codigo/Sitio/Sitio/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sitio.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
